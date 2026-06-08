8255a0ec44da42f28dd4dc52200d4e11a702f282
AbdelRahman Elrefai <team.man00@gmail.com>
2026-06-08 17:34:11 +0300

    Milestone E: Implemented Rich Entities (Portfolio, Asset) and Domain Interfaces

---
+++ Domain/Entities/Asset.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace PortfolioManagement.Domain.Entities
+{
+    public class Asset
+    {
+        public Guid Id { get; private set; }
+        public string TickerSymbol { get; private set; }
+        public decimal Quantity { get; private set; }
+        public decimal CurrentPrice { get; private set; }
+
+        private Asset() { }
+        public Asset(string tickerSymbol, decimal initialQuantity , decimal currentPrice)
+        {
+            if (string.IsNullOrEmpty(tickerSymbol))
+                throw new ArgumentException("Ticker Symbol cannot be empty.");
+            if (initialQuantity < 0)
+                throw new ArgumentException("Initial Price cannot be negative");
+            Id = Guid.NewGuid();
+            TickerSymbol = tickerSymbol;
+            Quantity = initialQuantity;
+            CurrentPrice = currentPrice;
+        }
+
+        public void UpdatePrice(decimal newPrice)
+        {
+            if (newPrice < 0)
+                throw new ArgumentException("New price cannot be negative");
+                CurrentPrice = newPrice;
+        }
+
+        public void AddQuantity(decimal newQuantity)
+        {
+            if (newQuantity <= 0)
+                throw new ArgumentException("New quantity cannot be negative or equal to zero");
+            Quantity += newQuantity;
+        }
+
+    }
+}
---
+++ Domain/Entities/Portfolio.cs
@@ -0,0 +1,42 @@
+﻿namespace PortfolioManagement.Domain.Entities
+{
+    public class Portfolio
+    {
+        public Guid Id { get; private set; }
+        public string UserId { get; private set; }
+        public string Name { get; private set; }
+
+        private readonly List<Asset> _assets = new();
+
+        public IReadOnlyCollection<Asset> Assets => _assets.AsReadOnly();
+
+        private Portfolio() { }
+        public Portfolio(string userId, string name)
+        {
+            if (string.IsNullOrEmpty(userId)) throw new ArgumentNullException("You must provide user id");
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Name = name;
+        }
+
+        public void AddAsset(Asset asset)
+        {
+            // check if we already have this ticker 
+            var existingAsset = _assets.FirstOrDefault(a => a.TickerSymbol == asset.TickerSymbol);
+            if(existingAsset != null)
+            {
+                existingAsset.AddQuantity(asset.Quantity);
+                existingAsset.UpdatePrice(asset.CurrentPrice);
+            }
+            else
+            {
+                _assets.Add(asset);
+            }
+        }
+
+        public decimal GetTotalValue()
+        {
+            return _assets.Sum(a => a.Quantity * a.CurrentPrice);
+        }
+    }
+}
